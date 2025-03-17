01292268e08b09987540ccd56f51dff9afa5018c
DanielGuth <carlosdanielcotticaguth@gmail.com>
2025-03-17 09:01:48 -0300

    Fazendo o update no método getDate para getUTCDate

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     function formatDate(date) {
         const d = new Date(date);
-        const day = String(d.getDate()).padStart(2, '0');
+        const day = String(d.getUTCDate()).padStart(2, '0');
         const month = String(d.getMonth() + 1).padStart(2, '0');
         const year = d.getFullYear();
         return `${day}/${month}/${year}`;
@@ -104,11 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <hr>
                 <p><strong>Hora: ${document.getElementById("hora").value}</strong></p>
                 <hr>
-                <p><strong>Responsável: ${document.getElementById("responsavel").value}</strong></p>
-
-                  
-            
-                
+                <p><strong>Responsável: ${document.getElementById("responsavel").value}</strong></p>        
             
             </body>
             </html>
